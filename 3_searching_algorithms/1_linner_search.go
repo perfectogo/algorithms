bd7b2d671a4ce2625a181b1d3552eb87c99d0a6b
perfectogo <jasurbek.go.dev@gmail.com>
2022-04-15 17:49:38 +0500

    searching algorithms changed

--- 3_searching_algorithms/1_linner_search.go
+++ 3_searching_algorithms/1_linner_search.go
@@ -4,23 +4,32 @@ import (
 	"fmt"
 )
 
-func main () {
-	var index, steps int
-	words := []string {"stol", "stul", "oynak", "ko'zgu"}
-	word := "stul"
+type numberic interface{
+	int | float64
+}
 
-	for i, w := range words {
+func LinerSearch[T comparable](element T, elements []T) int {
+	var steps int
+
+	for i, e := range elements {
 		steps++
-		if word == w {
-			index = i
+		if element == e {
+			return i
 			break
 		}
 	}
+	return 0
+}
 
-	if words[index] == word {
-		fmt.Println("mission completed, steps:", steps)
-		return
-	}
+func main() {
+	
+	numbers := []int{1, 2, 3, 4, 5, 6, 7}
+	number := 3
+	index := LinerSearch[int](number, numbers)
+	fmt.Println(index)
 
-	fmt.Println("mission not completed, steps:", steps)
+	words := []string{"stol", "stul", "oynak", "ko'zgu"}
+	word := "oynak"
+	index = LinerSearch[string](word, words)
+	fmt.Println(index)
 }
--- 3_searching_algorithms/2_binary_search.go
+++ 3_searching_algorithms/2_binary_search.go
@@ -4,32 +4,25 @@ import (
 	"fmt"
 )
 
-func main () {
-	var (
-		numbers []int = []int {12, 24, 45, 57, 68, 79, 80, 99, 121}
-		low, high, number int = 0, len(numbers) - 1, 80
-		middle, index, steps int
-	)
-
-	for /*i := low; i < high; i++ && */low <= high {
-		steps++
+func BinarySearch(number int, numbers []int) int {
+	var low, high int = 0, len(numbers)
+	var middle int
+	for low <= high {
 		middle = (low + high) / 2
-		fmt.Println(middle)
-
 		item := numbers[middle]
 		if item == number {
-			index = middle
-			break
-		} else if item < number {
+			return middle
+		} else if item > number {
 			high = middle - 1
 		} else {
 			low = middle + 1
 		}
-
 	}
-
-	fmt.Println("index", index)
-	fmt.Println("item", numbers[index])
-	fmt.Println("number" ,number)
-	fmt.Println("steps", steps)
+	return 0
+}
+func main() {
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 1000}
+	number := 1000
+	index := BinarySearch(number, numbers)
+	fmt.Println(index)
 }
